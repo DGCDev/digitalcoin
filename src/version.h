630cedf504a172ca22a5da9bd225dd97a0999da0
ahmedbodi <ABodiwala@uclan.ac.uk>
2014-11-17 19:10:26 +0000

    Update version and Minimum client version

--- src/version.h
+++ src/version.h
@@ -26,13 +26,13 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 2000001;
+static const int PROTOCOL_VERSION = 2000002;
 
 // intial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 2000002;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 2000001;
+static const int MIN_PEER_PROTO_VERSION = 2000002;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this

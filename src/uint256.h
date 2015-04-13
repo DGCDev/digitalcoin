a97c52f0cddce56524518f58da27d3a3e27f5b85
xawksow <xawksow@gmail.com>
2015-04-13 14:36:55 +0200

    Exception instead of assigning 0 in case of wrong vector length

--- src/uint256.h
+++ src/uint256.h
@@ -1,11 +1,14 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2013 The Bitcoin developers
+// Copyright (c) 2009-2015 The Bitcoin developers
+// Copyright (c) 2015 The Digitalcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_UINT256_H
 #define BITCOIN_UINT256_H
 
+#include <assert.h>
+#include <stdexcept>
 #include <stdint.h>
 #include <stdio.h>
 #include <string>
@@ -18,6 +21,11 @@ inline signed char HexDigit(char c)
 {
     return p_util_hexdigit[(unsigned char)c];
 }
+
+class uint_error : public std::runtime_error {
+public:
+    explicit uint_error(const std::string& str) : std::runtime_error(str) {}
+};
 
 /** Template base class for unsigned big integers. */
 template<unsigned int BITS>
@@ -62,11 +70,9 @@ public:
 
     explicit base_uint(const std::vector<unsigned char>& vch)
     {
-        if (vch.size() == sizeof(pn)) {
-            memcpy(pn, &vch[0], sizeof(pn));
-        } else {
-            *this = 0;
-        }
+        if (vch.size() != sizeof(pn))
+            throw uint_error("Converting vector of wrong size to base_uint");
+		memcpy(pn, &vch[0], sizeof(pn));
 	}
 
     bool operator!() const

9112559164b1bfb4013ec4e48c220120196ef520
xawksow <xawksow@gmail.com>
2015-04-13 17:07:52 +0200

    [Qt] Fix Transaction details shows wrong To:

--- src/qt/transactiondesc.h
+++ src/qt/transactiondesc.h
@@ -18,7 +18,7 @@ class TransactionDesc: public QObject
     Q_OBJECT
 
 public:
-    static QString toHTML(CWallet *wallet, CWalletTx &wtx, int vout, int unit);
+    static QString toHTML(CWallet *wallet, CWalletTx &wtx, TransactionRecord *rec, int unit);
 
 private:
     TransactionDesc() {}

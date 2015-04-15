65d380bce84bb3018f08a1ff284d0d1d352f4d35
xawksow <xawksow@gmail.com>
2015-04-16 00:00:10 +0200

    Relay double-spends, subject to anti-DOS Allows network wallets and other clients to see transactions that respend a prevout already spent in an unconfirmed transaction in this node's mempool.
    Knowledge of an attempted double-spend is of interest to recipients of the
    first spend.  In some cases, it will allow these recipients to withhold
    goods or services upon being alerted of a double-spend that deprives them
    of payment.
    
    As before, respends are not added to the mempool.
    
    Anti-Denial-of-Service-Attack provisions:
     - Use a bloom filter to relay only one respend per mempool prevout
     - Rate-limit respend relays to a default of 100 thousand bytes/minute
     - Define tx2.IsEquivalentTo(tx1): equality when scriptSigs are not considered
     - Do not relay these equivalent transactions
    
    Remove an unused variable declaration in txmempool.cpp.

--- src/main.h
+++ src/main.h
@@ -124,6 +124,9 @@ struct CNodeStateStats;
 
 struct CBlockTemplate;
 
+/** Set up internal signal handlers **/
+void RegisterInternalSignals();
+
 /** Register a wallet to receive updates from core */
 void RegisterWallet(CWalletInterface* pwalletIn);
 /** Unregister a wallet from core */

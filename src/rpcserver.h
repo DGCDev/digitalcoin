02988694dac472b57cf1397ca22797404021f766
xawksow <xawksow@gmail.com>
2015-04-16 00:24:10 +0200

    Replace HexBits with strprintf

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -118,7 +118,6 @@ extern int64_t nWalletUnlockTime;
 extern int64_t AmountFromValue(const json_spirit::Value& value);
 extern json_spirit::Value ValueFromAmount(int64_t amount);
 extern double GetDifficulty(const CBlockIndex* blockindex, int algo);
-extern std::string HexBits(unsigned int nBits);
 extern std::string HelpRequiringPassphrase();
 extern std::string HelpExampleCli(std::string methodname, std::string args);
 extern std::string HelpExampleRpc(std::string methodname, std::string args);

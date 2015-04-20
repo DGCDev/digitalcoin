5c605904b21ce8fa3e7f3d58644c8ab1ab65b9f6
xawksow <xawksow@gmail.com>
2015-04-20 05:45:08 +0200

    Fixes ignored qt 4.8 codecs path on windows when configuring with --w ith-qt-libdir

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -489,6 +489,7 @@ int main(int argc, char *argv[])
 
     Q_INIT_RESOURCE(bitcoin);
 	Q_INIT_RESOURCE(bitcoin_locale);
+	
     GUIUtil::SubstituteFonts();
 
     BitcoinApplication app(argc, argv);
